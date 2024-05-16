e4e0afc25673111e53134c771db74ffa0f3e2a14
aboutmydreams <aboutmydreams@163.com>
2024-05-16 20:50:38 +0800

    update version name

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="way3",
-    version="0.0.13",
+    version="0.0.15",
     author="aboutmydreams",
     author_email="aboutmydreams@163.com",
     description="Simplified file path management for Python developers",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="your_github_url",
+    url="https://github.com/aboutmydreams/way3",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
